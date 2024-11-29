3b487827dd12787376e3ae56a693ea423f560274
raave-aires <raave.aires@proton.me>
2024-11-29 14:25:10 -0300

    wip: primeiros passos

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
+import { ModeToggle } from "@/src/components/theme-toggle";
+import { MoveRight, Github } from "lucide-react";
+import { Button } from "@/src/components/ui/button";
+import { Badge } from "@/src/components/ui/badge";
 import Image from "next/image";
+import Link from "next/link";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/src/components/ui/tooltip"
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full py-20 lg:py-20">
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 gap-8 items-center md:grid-cols-2">
+        <div className="flex gap-4 flex-col">
+          <div>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="cursor-help">
+                <Badge variant="destructive">Alerta de spoilers!</Badge>
+              </TooltipTrigger>
+              <TooltipContent className="w-80 bg-background border text-white hyphens-auto ml-4">
+                <p>O conteúdo desta enciclopédia contém revelações significativas sobre todas as obras do Brandon Sanderson. Leia por sua conta e risco.</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            
+          </div>
+          <div className="flex gap-4 flex-col">
+            <h1 className="text-5xl md:text-7xl max-w-lg tracking-tighter text-left font-regular">
+              bem-vindo(a) à <span className="text-[#e73720]">sandbr</span>!
+            </h1>
+            <p className="text-xl leading-relaxed tracking-tight text-muted-foreground max-w-md text-left">
+              A enciclopédia brasileira sobre <Link href="/brandon-sanderson" className="hover:underline">Brandon Sanderson</Link>.
+            </p>
+          </div>
+          <div className="flex flex-row gap-4">
+            <Button size="lg" className="gap-4" variant="outline" asChild>
+              <a href="https://github.com/sand-br/sandbr" target="_blank" rel="noopener noreferrer">Contribua <GitHubLogoIcon className="w-4 h-4" /></a>
+            </Button>
+            <Button size="lg" className="gap-4">
+              Explorar <MoveRight className="w-4 h-4" />
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="grid grid-cols-2 gap-8">
+          <div className="bg-muted rounded-md aspect-square box-border overflow-hidden">
+            <Image src="https://i.imgur.com/1y5z29V.jpeg" width={300} height={300} alt="Vin"/>
+          </div>
+          <div className="bg-muted rounded-md row-span-2 aspect-[9/16] box-border overflow-hidden">
+            <Image src="https://i.imgur.com/dpPAk60.jpeg" width={500} height={1000} alt="Vin"/>
+          </div>
+          <div className="bg-muted rounded-md aspect-square box-border overflow-hidden">
+            <Image src="https://i.imgur.com/AxXqYO2.jpeg" width={300} height={300} alt="Vin"/>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,27 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploads.coppermind.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'awvvhcrihm7n6tuv.public.blob.vercel-storage.com'
+      }
+    ],
+  },
+}
+
 export default nextConfig;
