261eaf0d8d20a567f1b0cd78506b40e14071f47a
raave-aires <raave.aires@proton.me>
2024-12-01 02:44:47 -0300

    refatoração

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Moon, MoonIcon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/src/components/ui/button"
@@ -12,12 +12,12 @@ import {
   DropdownMenuTrigger,
 } from "@/src/components/ui/dropdown-menu"
 
-export function ModeToggle() {
+export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild >
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -26,13 +26,13 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Claro
+          Tema claro
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Escuro
+          Tema escuro
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          Sistema
+          Seguir o sistema
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
